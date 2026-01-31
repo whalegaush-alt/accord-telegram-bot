be053242e2b2650fdcc134e75c1c07cf6b398990
whalegaush-alt <whale.gaush@gmail.com>
2026-01-31 13:28:40 +0100

    bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Бот онлайн и работает 👍")
+    await update.message.reply_text("✅ Бот онлайн и работает")
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
